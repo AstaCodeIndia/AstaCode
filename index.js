63236a1240dc3d95673223646031c8457535d30b
Sumit Sharma <astacodeindia@gmail.com>
2024-04-05 11:13:48 +0530

    Robots

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.get('/normal', (req, res) => {
     res.status(200).render('mainframe.ejs', params);
 })
 app.get('/robots.txt', function (req, res) {
-    const params = {};
-    res.status(200).render('robots.txt', params);
+    res.type('text/plain');
+    res.send("User-agent: *\nDisallow:/normal");
 });
 
 
